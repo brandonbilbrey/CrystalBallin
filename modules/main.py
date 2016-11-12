8ebc08456c2185b7311ddda323894ed8cb32b8b0
cnorick <cnathanorick@gmail.com>
2016-11-12 16:32:52 -0600

    Fix bugs and get trainer to runnable state

--- modules/main.py
+++ modules/main.py
@@ -1,24 +1,25 @@
-import genetics, statsinterface
+import genetics as gene, statsinterface as si
+
+def train():
+    GameList = si.getGamesList()
+    Solution = gene.getSolution(GameList)
+    for attr in Solution.__dict__:
+        print "{0:40} {1:10}".format(attr,Solution.__dict__[attr])
+
+
+def predict():
+    pass
+
 
 if __name__ == '__main__':
     flags_dict = {"-v": 2}
     flags = 0
     
     while(1):
-        print("t - Train\np - predict")
-        user_input = input().split()
-        if(user_input[0] == 't'):
+        print "t - Train\np - predict"
+        user_input = raw_input().split()
+        if(user_input[0] == "t"):
             train()
-        elif(user_input[0] == 'p'):
+        elif(user_input[0] == "p"):
             pass
 
-
-def train():
-    GameList = getGamesList()
-    Solution = getSolution(GameList)
-    for attr, val in Solution.__dict__:
-        print "{0:40} {1:10}".format(attr,val)
-
-                
-def predict():
-    pass
