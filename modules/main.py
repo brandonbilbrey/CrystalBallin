59a518a4e39135a399ddefe381c47890e29908ff
osillyscope <brandoncb95@gmail.com>
2016-11-12 15:48:17 -0500

    Started working on main.py.

--- modules/main.py
+++ modules/main.py
@@ -1,2 +1,17 @@
+import sys
+import genetics, statsinterface
+
 if __name__ == '__main__':
-    print("Hello World!")
+    flags_dict = {"-v": 2}
+    flags = 0
+    
+    while(1):
+        print("t - Train\np - predict")
+        user_input = input().split()
+        
+
+def train():
+    pass 
+
+def predict():
+    pass
